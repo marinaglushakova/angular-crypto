a0fde55a27e4e6d147ae3d00587124ad7ffc2953
Marina Glushakova <marinag@chelyabinsk.fuse8.local>
2023-02-10 10:41:33 +0500

    adds show graph logic

--- src/app/components/graph/graph.component.ts
+++ src/app/components/graph/graph.component.ts
@@ -5,21 +5,83 @@ import {
   Output,
   EventEmitter,
   SimpleChange,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  OnDestroy,
 } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { TickerPriceService } from '../../services/ticker-price.service';
 
 @Component({
   selector: 'app-graph',
   templateUrl: './graph.component.html',
 })
-export class GraphComponent implements OnChanges {
+export class GraphComponent implements OnChanges, AfterViewInit, OnDestroy {
   @Input() tickerToShow: string;
   @Output() closeGraphEvent = new EventEmitter();
+  @ViewChild('graph') graphDiv: ElementRef;
+  @ViewChild('column') columnDiv: ElementRef;
+  graphArray: number[] = [];
+  normalizedGraph: number[] = [];
+  subscription: Subscription;
+  maxGraphElements = 1;
 
-  ngOnChanges(changes: { tickerToShow: SimpleChange }) {
-    console.log(this.tickerToShow);
+  constructor(private tickerPriceService: TickerPriceService) {}
+
+  ngOnChanges(changes: { tickerToShow: SimpleChange }): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    this.graphArray = [];
+    this.showGraph();
   }
 
-  onCloseGraphClick() {
+  ngAfterViewInit(): void {
+    this.calculateMaxGraphElements();
+    window.addEventListener('resize', this.calculateMaxGraphElements);
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('resize', this.calculateMaxGraphElements);
+    this.subscription.unsubscribe();
+  }
+
+  onCloseGraphClick(): void {
+    this.subscription.unsubscribe();
     this.closeGraphEvent.emit();
+  }
+
+  showGraph(): void {
+    this.subscription = this.tickerPriceService
+      .getPrice(this.tickerToShow)
+      .subscribe((price: number) => {
+        this.graphArray.push(price);
+        this.normalizeGraph();
+        while (this.graphArray.length > this.maxGraphElements) {
+          this.graphArray.shift();
+        }
+      });
+  }
+
+  normalizeGraph(): void {
+    const maxValue = Math.max(...this.graphArray);
+    const minValue = Math.min(...this.graphArray);
+
+    if (maxValue === minValue) {
+      this.normalizedGraph = this.graphArray.map(() => 50);
+      return;
+    }
+
+    this.normalizedGraph = this.graphArray.map(
+      (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
+    );
+  }
+
+  calculateMaxGraphElements(): void {
+    const graphWidth = this.graphDiv?.nativeElement.clientWidth;
+    const columnWidth = this.columnDiv?.nativeElement.clientWidth ?? 38;
+
+    this.maxGraphElements = graphWidth / columnWidth;
   }
 }
